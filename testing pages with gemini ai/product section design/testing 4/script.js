bace8e69850e16cb8f664599fc8d9fb633ddd697
Adam-TechStudent <akshayadam2995@gmail.com>
2026-06-26 21:51:57 -0500

    testing the product section with multiple design

---
+++ testing pages with gemini ai/product section design/testing 4/script.js
@@ -0,0 +1,13 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const Linen = document.querySelectorAll("#linen-btn");
+    const Cotton = document.querySelectorAll("#cotton-btn");
+    const Wool = document.querySelectorAll("#wool-btn");
+    const Silk = document.querySelectorAll("#silk-btn");
+    const Denim = document.querySelectorAll("#denim-btn");
+    const Trouser = document.querySelectorAll("#trouser-btn");
+
+    document.getElementById("linen-btn")
+
+
+
+});
